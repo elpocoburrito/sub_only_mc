8cc687fc38408c7532adfac59ebc69ec3dae89c4
rockbyo5 <rockbyo5@gmail.com>
2020-05-04 15:39:32 -0400

    gdf

--- src/main/java/elpocoburrito/subonly/PlayerAllowedMoveEvent.java
+++ src/main/java/elpocoburrito/subonly/PlayerAllowedMoveEvent.java
@@ -1,5 +1,8 @@
 package elpocoburrito.subonly;
 
+import com.alessiodp.lastloginapi.api.LastLogin;
+import com.alessiodp.lastloginapi.api.interfaces.LastLoginAPI;
+import com.alessiodp.lastloginapi.api.interfaces.LastLoginPlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -14,7 +17,19 @@ public class PlayerAllowedMoveEvent implements Listener {
 
     @EventHandler
     public void onPlayerMoveEvent(PlayerMoveEvent event){
+        if (javaplugin.getServer().getPluginManager().getPlugin("LastLoginAPI") != null) {
+            if (javaplugin.getServer().getPluginManager().getPlugin("LastLoginAPI").isEnabled()) {
+                LastLoginAPI api = LastLogin.getApi();
+
+                LastLoginPlayer player = api.getPlayer(event.getPlayer().getUniqueId());
+                event.getPlayer().sendMessage(String.valueOf(player.getLastLogin()));
+                String name = player.getName(); // Get the name
+            }
+        }
         if (!event.getPlayer().hasPermission("subonly.subbed") && javaplugin.getConfig().getBoolean("enabled"))
             event.setCancelled(true);
+        else {
+            event.getPlayer().kickPlayer("You are not subbed");
+        }
     }
 }
